1fb0879a9ba804550c7226d3b9baf7b0dcd197b6
Amjad Shaaban <theamjad@outlook.com>
2019-09-19 00:08:59 -0400

    Final Push before Submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const myTopics = [
+  var myTopics = [
     "James Webb Telescope",
     "World of Warcraft",
     "George Carlin",
@@ -11,58 +11,61 @@ $(document).ready(function() {
     "Javascript",
     "EAGLES!"
   ];
+
   function createTopicBtn(name) {
     var topicBtn = $("<button>");
-    topicBtn.attr("class", "topic-btn");
-    topicBtn.attr("data", name);
-    topicBtn.text(name);
-    topicBtn.appendTo($(".topic-btns"));
+    topicBtn
+      .attr({
+        class: "topic-btn",
+        data: name
+      })
+      .text(name)
+      .appendTo($(".topic-btns"));
     $(topicBtn).on("click", function(event) {
       event.preventDefault();
-      var queryTerm = $(this).attr("data");
-      console.log(queryTerm);
-      var queryURL =
-        "https://api.giphy.com/v1/gifs/search?q=" +
-        queryTerm +
-        "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+      var queryTerm = $(this).attr("data"),
+        limit = 10,
+        queryURL =
+          "https://api.giphy.com/v1/gifs/search?q=" +
+          queryTerm +
+          "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=" +
+          limit;
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response);
-        var counter = 0;
-        var results = [];
+        var counter = 0,
+          results = [];
         results[counter] = response.data;
 
         for (var i = 0; i < results[counter].length; i++) {
-          var gifDiv = $("<div>");
-          gifDiv.attr("class", "card");
-          var rating = results[counter][i].rating;
-          console.log(results[counter][i]);
-          var resultImage = $("<img>");
-          var cardBody = $("<div>");
-          var p = $("<p>");
-          p.attr("class", "card-text");
-          p.text("Rating: " + rating);
-          resultImage.attr("class", "card-img-top");
-          resultImage.attr("arr-x", counter);
-          resultImage.attr("arr-y", i);
-          resultImage.attr(
-            "src",
-            results[counter][i].images.original_still.url
-          );
-          resultImage.attr("state", "still");
-          gifDiv.prepend(p);
-          gifDiv.prepend(cardBody);
-          gifDiv.prepend(resultImage);
-          $(".results-area").prepend(gifDiv);
-          $(resultImage).on("click", function() {
-            var state = $(this).attr("state");
-            var x = $(this).attr("arr-x");
-            var y = $(this).attr("arr-y");
+          var gifDiv = $("<div>").attr("class", "card"),
+            rating = results[counter][i].rating,
+            resultImage = $("<img>"),
+            cardBody = $("<div>"),
+            p = $("<p>");
 
-            var still = results[x][y].images.original_still.url;
-            var animated = results[x][y].images.original.url;
+          p.attr("class", "card-text").text("Rating: " + rating);
+
+          resultImage.attr({
+            class: "card-img-top",
+            "arr-x": counter,
+            "arr-y": i,
+            src: results[counter][i].images.original_still.url,
+            state: "still"
+          });
+
+          gifDiv.prepend(p, cardBody, resultImage);
+
+          $(".results-area").prepend(gifDiv);
+
+          $(resultImage).on("click", function() {
+            var state = $(this).attr("state"),
+              x = $(this).attr("arr-x"),
+              y = $(this).attr("arr-y"),
+              still = results[x][y].images.original_still.url,
+              animated = results[x][y].images.original.url;
+
             if (state === "still") {
               $(this).attr("src", animated);
               $(this).attr("state", "animated");
@@ -83,6 +86,8 @@ $(document).ready(function() {
   }
   $(".add-topic-btn").on("click", () => {
     var newTopic = $(".new-topic-box").val();
+    myTopics.push(newTopic);
+    console.log(myTopics);
     createTopicBtn(newTopic);
   });
 });
